a8d741160930e26fd3fc60ca922d14a57742d27e
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-09-18 20:11:05 -0300

    Refatorando - class Pessoa

--- src/br/com/house/digital/exercicio1/Pessoa.kt
+++ src/br/com/house/digital/exercicio1/Pessoa.kt
@@ -2,9 +2,9 @@ package br.com.house.digital.exercicio1
 
 class Pessoa(var nome:String, var rg: Int) {
     override fun equals(other: Any?): Boolean {
-//        if (other !is Pessoa) return false
-//        return (this.rg == other.rg)
-        other as Pessoa
-        return rg == other.rg
+        if (other !is Pessoa) return false
+        return (this.rg == other.rg)
+//        other as Pessoa
+//        return rg == other.rg
     }
 }
